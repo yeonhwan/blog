921feefc71f44ecf6939a896e001d5eea9d63898
yeonhwan <yeonhwan619@gmail.com>
2025-04-17 20:44:53 +0900

    Update posts page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,23 +1,29 @@
-import { getAllPosts } from "@/actions/posts";
+import { getAllPosts, getAllTags } from "@/actions/posts";
 import PageTitle from "@/components/ui/PageTitle";
 import PostItem from "@/components/ui/posts/PostItem";
+import TagSelector from "@/components/ui/posts/TagSelector";
 import Pagination from "@/components/ui/posts/Pagination";
 
 type PostsProps = {
   searchParams: {
     page?: string;
-    sort?: "ASC" | "DESC";
+    tag?: string;
   };
 };
 
 export default async function Posts({ searchParams }: PostsProps) {
   const params = await searchParams;
   const page = params.page ? Number(params.page) : (1 as number);
-  const { total, data: posts } = await getAllPosts({ page });
+  const tag = params.tag;
+  const { total, data: posts } = await getAllPosts({ page, tag });
+  const tags = await getAllTags();
 
   return (
     <div>
       <PageTitle title="Posts" />
+      <div className="flex w-full justify-end mb-10">
+        <TagSelector key={tag} current={tag} data={tags} />
+      </div>
       <ul className="flex flex-col gap-6">
         {posts.map(({ data }, idx) => (
           <PostItem key={data.slug} {...data} />
