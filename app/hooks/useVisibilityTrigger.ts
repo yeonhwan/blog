fc18197333069c7920eb238404be23ebe8fb9540
yeonhwan <yeonhwan619@gmail.com>
2025-04-17 20:43:15 +0900

    Add new hook to control tagselector component

---
+++ app/hooks/useVisibilityTrigger.ts
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from "react";
+
+type VisibiltyTriggerOption = {
+  onVisible?: () => void;
+  onHidden?: () => void;
+};
+
+export const useVisibilityTrigger = <T extends HTMLElement>(
+  opts: IntersectionObserverInit & VisibiltyTriggerOption,
+  dependencies: any[] = [],
+) => {
+  const targetRef = useRef<T | null>(null);
+  const prevVisibility = useRef<boolean>(false);
+  const { onVisible, onHidden, ...options } = opts;
+
+  const intersectionCallback: IntersectionObserverCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting === prevVisibility.current) return;
+      if (entry.isIntersecting) {
+        prevVisibility.current = true;
+        onVisible?.();
+      } else {
+        prevVisibility.current = false;
+        onHidden?.();
+      }
+    });
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(intersectionCallback, opts);
+    if (!targetRef.current) return;
+
+    observer.observe(targetRef.current);
+  }, [targetRef.current, ...dependencies]);
+
+  return targetRef;
+};
