5e88fea58938d069546e03e4d96b49de09fc01c0
yeonhwan <yeonhwan619@gmail.com>
2025-04-20 19:13:34 +0900

    Refactor markdown css into separate file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./markdown.css";
 import { FiraCode, NbgKR } from "./fonts/fonts";
 import Header from "@/components/ui/Header";
 
