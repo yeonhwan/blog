64ce2fa3590c9349ef4e5416293105ea55baf31e
yeonhwan <yeonhwan619@gmail.com>
2025-04-22 15:40:33 +0900

    Add preload to fonts

--- app/fonts/fonts.ts
+++ app/fonts/fonts.ts
@@ -2,12 +2,14 @@ import localFont from "next/font/local";
 
 // Fira Code Variable
 export const FiraCode = localFont({
+  preload: true,
   src: "./FiraCode.woff2",
   variable: "--font-fira",
 });
 
 // 나눔 바른 고딕
 export const NbgKR = localFont({
+  preload: true,
   src: [
     {
       path: "./NBG_bold.woff2",
