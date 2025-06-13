ed26046e949c88456bc63b28b8a43ad784fca3be
yeonhwan <yeonhwan619@gmail.com>
2025-06-13 19:47:53 +0900

    Change xml settings for not collecting unpublished settings

--- app/server-sitemap.xml/route.ts
+++ app/server-sitemap.xml/route.ts
@@ -3,7 +3,9 @@ import { getAllPosts } from "@/actions/posts";
 
 export async function GET() {
   const { data } = await getAllPosts({ ssg: true });
-  const slugs = data.map((item) => item.post.data.slug);
+  const slugs = data
+  .filter(item => item.post.data.publish)
+  .map((item) => item.post.data.slug)
 
   const sitemaps = slugs.map((slug) => ({
     loc: `https://yeonhwandev.com/posts/${slug}`,
